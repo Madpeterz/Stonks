481f817117be1eb556cb61b7aa77bc5246cd0b0e
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2026-09-11 19:47:40 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -33,7 +33,7 @@ local TEX_CHECKED_PATH = constants.folderPath .. "images/" .. TEX_CHECKED
 local Stonks = {
 	name = "Stonks",
 	author = "Madpeter",
-	version = "1.0.1",
+	version = "1.0.2",
 	desc = "Its worth 1000g im sure"
 }
 
